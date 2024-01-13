ecf7363b78db3d8946021a35466c51b0a9886f05
AnasBarakat01 <155667484+AnasBarakat01@users.noreply.github.com>
2024-01-13 17:22:04 +0200

    Add files via upload

---
+++ create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('gender',['male','female']);
+            $table->date('birthDate');
+            $table->string('profilePhoto',2048)->nullable();
+            $table->string('region');
+            $table->string('city');
+            $table->string('phone');
+            $table->integer('my_balance')->defalt(0);
+            $table->string('activation_code',2048);
+            $table->date('activation_expiry')->nullable();
+            $table->date('activated_at')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
