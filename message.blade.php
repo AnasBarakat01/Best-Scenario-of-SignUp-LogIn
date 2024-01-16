e925a69e7abc5ce7e842b366a7b596180e23f6ca
AnasBarakat01 <155667484+AnasBarakat01@users.noreply.github.com>
2024-01-16 14:44:12 +0200

    Add files via upload

---
+++ message.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+<h1>Activate Your Account</h1>
+</head>
+<body>
+    <a href={{ $ss }}>Click here</a>
+<p>If you didn't sign up to this website</p>
+<p>, ignore this message</p>
+</body>
+</html>
